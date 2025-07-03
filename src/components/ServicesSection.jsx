408391cfe592e294c05e14571d15c07e19c76190
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-07-03 17:32:40 +0530

    1222

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -4,23 +4,7 @@ import ScrollFloat from "./react-bits/ScrollFloat";
 const ServicesSection = () => {
   return (
     <>
-      {/* <div className="min-h-screen w-full h-full flex justify-center text-white">
-        <div>
-          <ScrollFloat
-            animationDuration={1}
-            ease="back.inOut(2)"
-            scrollStart="center bottom+=50%"
-            scrollEnd="bottom bottom-=40%"
-            stagger={0.03}
-          >
-            We Offer Something more !
-          </ScrollFloat>
-        </div>
-
-        
-      </div> */}
-
-      
+      <div className="">We Offer Something more !</div>
     </>
   );
 };
