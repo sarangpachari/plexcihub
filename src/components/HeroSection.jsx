ff7e67e4f6ca01035382746a2da2521c48198a24
SARANG.P.ACHARI <58848506+sarangpachari@users.noreply.github.com>
2025-08-16 12:59:40 +0530

    home landing started

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import BlurText from "./react-bits/BlurText";
+import Landing1 from "../../public/Landing1.png"
 
 const HeroSection = () => {
   return (
-    <div id="home" className="min-h-screen w-full flex justify-center items-center p-4 text-center text-black">
+    <div id="home" className="min-h-screen w-full flex md:flex-row flex-col justify-center items-center p-4 text-center text-black">
       <BlurText
         text="Isn't this so cool?!"
         delay={700}
@@ -11,6 +12,7 @@ const HeroSection = () => {
         direction="bottom"
         className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold text-gray-400 leading-tight px-4 sm:px-8"
       />
+      <img src={Landing1} className="w-sm md:w-lg lg:w-2xl" alt="" />
     </div>
   );
 };
